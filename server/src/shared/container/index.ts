16e2b4d8b6b3d2b8bdb03af556e239b35ff399fc
Caio Tracera <ctrcra@gmail.com>
2021-02-22 21:09:29 -0300

    feat: add UsersRepository container

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -1,2 +1,12 @@
-import './providers';
+import { container } from 'tsyringe';
+
 import '@modules/users/providers';
+import './providers';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
