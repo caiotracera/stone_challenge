495836e43ec730c04223d5e4b0bddb6b6cbb5c32
Caio Tracera <ctrcra@gmail.com>
2021-02-22 21:27:47 -0300

    feat: add UpdateUserAvatar service

---
+++ server/src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+  avatarFilename: string;
+}
+
+@injectable()
+export default class UpdateUserAvatarService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
+  ) {}
+
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    if (user.avatar) {
+      await this.storageProvider.deleteFile(user.avatar);
+    }
+
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+    user.avatar = filename;
+
+    await this.usersRepository.save(user);
+    return user;
+  }
+}
