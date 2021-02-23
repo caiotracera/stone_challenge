282effa6ee20c1b00d7b4592a081de85d8eaaf74
Caio Tracera <ctrcra@gmail.com>
2021-02-23 18:44:32 -0300

    fix: fix error response

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
@@ -29,20 +29,21 @@ app.use(routes);
 app.use(errors());
 app.use(Sentry.Handlers.errorHandler());
 
-app.use((error: Error, request: Request, response: Response) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
 
-  console.log(error);
-
-  return response
-    .status(500)
-    .json({ status: 'error', message: 'Internal server error' });
-});
+    console.log(error);
+    return response
+      .status(500)
+      .json({ status: 'error', message: 'Internal server error' });
+  },
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
