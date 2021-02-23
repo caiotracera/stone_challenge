7751a9ca4b3bbf4c0751d382409cebfe0f42f813
Caio Tracera <ctrcra@gmail.com>
2021-02-22 22:07:56 -0300

    feat: add password routes

---
+++ server/src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { Segments, Joi, celebrate } from 'celebrate';
+
+import ForgotPasswordController from '@modules/users/infra/http/controllers/ForgotPasswordController';
+import ResetPasswordController from '@modules/users/infra/http/controllers/ResetPasswordController';
+
+const router = Router();
+const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
+
+router.post(
+  '/forgot',
+  celebrate({ [Segments.BODY]: { email: Joi.string().email().required() } }),
+  forgotPasswordController.create,
+);
+
+router.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
+);
+
+export default router;
--- server/src/shared/infra/http/routes/index.ts
+++ server/src/shared/infra/http/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+
 const routes = Router();
+routes.use('/password', passwordRouter);
 
 export default routes;
