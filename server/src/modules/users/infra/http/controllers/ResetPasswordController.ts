3826e82ef23249aab5bbc3f080732b862f345af8
Caio Tracera <ctrcra@gmail.com>
2021-02-22 22:07:16 -0300

    feat: add ResetPassword controller

---
+++ server/src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
+interface ICreateBody {
+  password: string;
+  token: string;
+}
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body as ICreateBody;
+
+    const resetPasswordService = container.resolve(ResetPasswordService);
+
+    await resetPasswordService.execute({ password, token });
+    return response.status(204).send();
+  }
+}
