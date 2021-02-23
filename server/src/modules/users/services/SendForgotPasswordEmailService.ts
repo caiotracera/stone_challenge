4a99eb2ee2644176a547dc236744d67e5f10cd19
Caio Tracera <ctrcra@gmail.com>
2021-02-22 21:56:15 -0300

    feat: add SendForgotPasswordEmail service

---
+++ server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,57 @@
+import { inject, injectable } from 'tsyringe';
+import { resolve } from 'path';
+
+import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+export default class SendForgotEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const { token } = await this.userTokensRepository.generate(user.id);
+
+    const forgotPasswordTemplate = resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.username,
+        email: user.email,
+      },
+      subject: '[CantWait] Password recovery',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.username,
+          link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
+        },
+      },
+    });
+  }
+}
