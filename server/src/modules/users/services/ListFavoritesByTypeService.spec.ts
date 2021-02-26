d86dd87beddfe15fa1eec4df819b5f3e108a3c86
Caio Tracera <ctrcra@gmail.com>
2021-02-26 16:12:39 -0300

    feat: add ListFavoritesByType service and tests

---
+++ server/src/modules/users/services/ListFavoritesByTypeService.spec.ts
@@ -0,0 +1,91 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeUserFavoritesRepository from '@modules/users/repositories/fakes/FakeUserFavoritesRepository';
+import ListFavoritesByTypeService from '@modules/users/services/ListFavoritesByTypeService';
+
+let fakeCacheProvider: FakeCacheProvider;
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserFavoritesRepository: FakeUserFavoritesRepository;
+let listFavoritesByTypeService: ListFavoritesByTypeService;
+
+describe('ListFavoritesByType', () => {
+  beforeEach(() => {
+    fakeCacheProvider = new FakeCacheProvider();
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserFavoritesRepository = new FakeUserFavoritesRepository();
+
+    listFavoritesByTypeService = new ListFavoritesByTypeService(
+      fakeUsersRepository,
+      fakeUserFavoritesRepository,
+      fakeCacheProvider,
+    );
+  });
+
+  it('should be able to list users favorites by type', async () => {
+    const user = await fakeUsersRepository.create({
+      username: 'johndoe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    });
+
+    const charOne = await fakeUserFavoritesRepository.add({
+      user_id: user.id,
+      type: 'characters',
+      favorite_id: 1,
+    });
+
+    const charTwo = await fakeUserFavoritesRepository.add({
+      user_id: user.id,
+      type: 'characters',
+      favorite_id: 2,
+    });
+
+    const charThree = await fakeUserFavoritesRepository.add({
+      user_id: user.id,
+      type: 'characters',
+      favorite_id: 3,
+    });
+
+    const comicOne = await fakeUserFavoritesRepository.add({
+      user_id: user.id,
+      type: 'comics',
+      favorite_id: 1,
+    });
+
+    const comicTwo = await fakeUserFavoritesRepository.add({
+      user_id: user.id,
+      type: 'comics',
+      favorite_id: 2,
+    });
+
+    const comicThree = await fakeUserFavoritesRepository.add({
+      user_id: user.id,
+      type: 'comics',
+      favorite_id: 3,
+    });
+
+    const charactersFavorites = await listFavoritesByTypeService.execute({
+      type: 'characters',
+      user_id: user.id,
+    });
+
+    const comicsFavorites = await listFavoritesByTypeService.execute({
+      type: 'comics',
+      user_id: user.id,
+    });
+
+    expect(charactersFavorites).toEqual([charOne, charTwo, charThree]);
+    expect(comicsFavorites).toEqual([comicOne, comicTwo, comicThree]);
+  });
+
+  it('should not be able to list users favorite if user does not exists', async () => {
+    await expect(
+      listFavoritesByTypeService.execute({
+        type: 'characters',
+        user_id: 'invalid_user_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ server/src/modules/users/services/ListFavoritesByTypeService.ts
@@ -0,0 +1,44 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserFavoritesRepository from '@modules/users/repositories/IUserFavoritesRepository';
+import IUserFavorite from '@modules/users/infra/typeorm/entities/UserFavorite';
+
+type IRequest = {
+  user_id: string;
+  type: 'characters' | 'comics';
+};
+
+@injectable()
+export default class ListFavoritesByTypeServi8ce {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserFavoritesRepository')
+    private userFavoritesRepository: IUserFavoritesRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({ type, user_id }: IRequest): Promise<IUserFavorite[]> {
+    const user = await this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const cacheKey = `favorites-${type}-${user_id}`;
+    let favorites = await this.cacheProvider.recover<IUserFavorite[]>(cacheKey);
+
+    /* istanbul ignore else */
+    if (!favorites) {
+      favorites = await this.userFavoritesRepository.listByType(type);
+      await this.cacheProvider.save({ key: cacheKey, value: favorites });
+    }
+
+    return favorites;
+  }
+}
