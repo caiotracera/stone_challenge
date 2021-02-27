5843a8cac951e57c87bd1af4366b271c279f91da
Caio Tracera <ctrcra@gmail.com>
2021-02-27 15:14:44 -0300

    chore: initial commit

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+import Logo from '../assets/images/Logo.svg'
+
+import { Container, LeftContainer, RightContainer } from '../styles/pages/index'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Login - Marvel Stone</title>
+      </Head>
+
+      <Container>
+        <LeftContainer>
+          <Logo />
+        </LeftContainer>
+        <RightContainer></RightContainer>
+      </Container>
+    </div>
+  )
+}
---
+++ web/src/styles/GlobalStyle.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  :root {
+    --white: #fff;
+    --background: #d71414;
+    --gray-line: #dcdde0;
+    --red: #e83f5b;
+  }
+  @media(max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+  @media(max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+  body {
+    background: var(--background);
+    color: var(--text);
+  }
+  body, input, textarea, button {
+    font: 400 1rem "Inter", sans-serif;
+  }
+  button {
+    cursor: pointer;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  .container {
+    height: 100vh;
+    max-width: 992px;
+    margin: 0 auto;
+    padding: 2.5rem 2rem;
+    display: flex;
+    flex-direction: column;
+  }
+`
