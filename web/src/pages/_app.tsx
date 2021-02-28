ef9c2160d7ab2a7bde606380c19270e5c5b3db39
Caio Tracera <ctrcra@gmail.com>
2021-02-28 12:28:04 -0300

    feat: add toasts

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/app'
 import GlobalStyle from '../styles/GlobalStyle'
 
+import AppProvder from '../hooks'
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <AppProvder>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </AppProvder>
   )
 }
 
