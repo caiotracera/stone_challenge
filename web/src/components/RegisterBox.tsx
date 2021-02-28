9ddd13b284aee4a52c9413878a03b9bfa81018ea
Caio Tracera <ctrcra@gmail.com>
2021-02-28 13:16:07 -0300

    chore: remove unused code

--- web/src/components/RegisterBox.tsx
+++ web/src/components/RegisterBox.tsx
@@ -95,8 +95,6 @@ export default function RegisterBox() {
         draggable
         pauseOnHover
       />
-      {/* Same as */}
-      <ToastContainer />
 
       <strong>Cadastre-se</strong>
       <p>Preencha os dados e crie sua conta</p>
