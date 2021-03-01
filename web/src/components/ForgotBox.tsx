8729747edff8692489495815bd3671a82023bbc6
Caio Tracera <ctrcra@gmail.com>
2021-02-28 23:00:33 -0300

    feat: add Forgot feature

--- web/src/components/ForgotBox.tsx
+++ web/src/components/ForgotBox.tsx
@@ -2,20 +2,87 @@ import { useCallback, useRef } from 'react'
 import Link from 'next/link'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
-import { FiMail, FiLock } from 'react-icons/fi'
+import { FiMail } from 'react-icons/fi'
+import * as Yup from 'yup'
+import { ToastContainer, toast } from 'react-toastify'
+import { useRouter } from 'next/router'
 
 import Input from '../components/Input'
 import Button from '../components/Button'
 import { Container, LinkContainer } from '../styles/components/ForgotBox'
+import api from '../services/api'
+import getValidationErrors from '../utils/getValidationErrors'
+import { GetServerSideProps } from 'next'
+
+type ForgotFormData = {
+  email: string
+}
 
 export default function ForgotBox() {
   const formRef = useRef<FormHandles>(null)
+  const router = useRouter()
 
-  const handleSubmit = useCallback(() => {
-    console.log()
+  const handleSubmit = useCallback(async (data: ForgotFormData) => {
+    try {
+      formRef.current?.setErrors({})
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('The email is required')
+          .email('The e-mail must be valid')
+      })
+
+      await schema.validate(data, { abortEarly: true })
+
+      await api.post('/password/forgot', {
+        email: data.email
+      })
+
+      toast.success('Token enviado com sucesso!', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+      })
+
+      setTimeout(() => {
+        router.push('/recover')
+      }, 5000)
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error)
+        formRef.current?.setErrors(errors)
+
+        return
+      }
+
+      toast.error(`${error.response.data.message}`, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+      })
+    }
   }, [])
   return (
     <Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <strong>Recuperar senha</strong>
       <p>
         Coloque o e-mail associado a sua conta. Em até 5 minutos enviaremos um
--- web/src/components/RecoverBox.tsx
+++ web/src/components/RecoverBox.tsx
@@ -1,38 +1,106 @@
 import { useCallback, useRef } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import { FiHash, FiLock } from 'react-icons/fi'
+import * as Yup from 'yup'
+import { ToastContainer, toast } from 'react-toastify'
 
 import Input from '../components/Input'
 import Button from '../components/Button'
 import { Container, LinkContainer } from '../styles/components/RecoverBox'
+import api from '../services/api'
+import getValidationErrors from '../utils/getValidationErrors'
+
+type RecoverFormData = {
+  token: string
+  password: string
+  password_confirmation: string
+}
 
 export default function RecoverBox() {
   const formRef = useRef<FormHandles>(null)
+  const router = useRouter()
 
-  const handleSubmit = useCallback(() => {
-    console.log()
+  const handleSubmit = useCallback(async (data: RecoverFormData) => {
+    try {
+      formRef.current?.setErrors({})
+
+      const schema = Yup.object().shape({
+        token: Yup.string().required('Token is required'),
+        password: Yup.string().required('Password is required'),
+        password_confirmation: Yup.string().oneOf(
+          [Yup.ref('password'), null],
+          'Passwords must match'
+        )
+      })
+
+      await schema.validate(data, { abortEarly: true })
+
+      await api.post('/password/reset', {
+        token: data.token,
+        password: data.password,
+        password_confirmation: data.password_confirmation
+      })
+
+      toast.success('Senha resetada com sucesso!', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+      })
+
+      setTimeout(() => {
+        router.push('/')
+      }, 5000)
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error)
+        formRef.current?.setErrors(errors)
+        return
+      }
+
+      toast.error(`${error.response.data.message}`, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+      })
+    }
   }, [])
   return (
     <Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
       <strong>Criar nova senha</strong>
 
       <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input autoFocus name="token" icon={FiHash} placeholder="Token" />
         <Input
-          autoFocus
-          name="token"
-          icon={FiHash}
-          placeholder="Código recebido no e-mail"
-        />
-        <Input
-          autoFocus
+          type="password"
           name="password"
           icon={FiLock}
           placeholder="Nova senha"
         />
         <Input
-          autoFocus
+          type="password"
           name="password_confirmation"
           icon={FiLock}
           placeholder="Confirmar nova senha"
