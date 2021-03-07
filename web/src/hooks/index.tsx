47527386f6d50c5301fce06b1864d2102a89a7fb
Caio Tracera <ctrcra@gmail.com>
2021-03-07 15:44:57 -0300

    chore: initial commit

--- web/src/hooks/index.tsx
+++
@@ -1,7 +0,0 @@
-import { AuthProvider } from './auth'
-
-const AppProvider: React.FC = ({ children }) => (
-  <AuthProvider>{children}</AuthProvider>
-)
-
-export default AppProvider
--- web/src/styles/pages/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.section`
-  flex: 1;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 6.25rem;
-  align-content: center;
-`
